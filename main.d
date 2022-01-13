8105b3ed703222989a2016c10a483f00f60d4436
csharpdf <83098445+csharpdf@users.noreply.github.com>
2022-01-12 23:34:46 -0500

    start

---
+++ main.d
@@ -0,0 +1,11 @@
+import std.net.curl, std.json;
+
+class database {
+    private string key;
+    private string endpoint;
+    this(string endpointt, keyy) {
+        key = keyy;
+        endpoint = endpointt;
+    }
+    
+}
